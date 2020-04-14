f3da63f3b4acd0fd15e09be83e6817c2ebb4dee9
Santiago Sanchez <48868506+santisanchez0@users.noreply.github.com>
2020-04-14 00:04:02 -0300

    Add files via upload
    commit 1

---
+++ queMePongo/src/main/java/queMePongo/ExceptionFaltaDescripcion.java
@@ -0,0 +1,11 @@
+package queMePongo;
+
+public class ExceptionFaltaDescripcion extends Exception {
+	public ExceptionFaltaDescripcion(String msg){
+	    super(msg);
+	}
+
+	public ExceptionFaltaDescripcion(){
+	    super();
+	}
+}
